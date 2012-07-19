24cea9830beb8de281d2226abfef85d9ccbcb5af
Pierre Marchand <pierre@oep-h.com>
2012-07-19 18:08:04 +0200

    fork

--- gevent_server.py
+++ gevent_server.py
@@ -25,10 +25,26 @@ def main():
 	parser.add_argument('-a', "--address", help="Address to bind to", default='')
 	parser.add_argument('-p', "--port", help="Port to listen to", default=8000, type=int)
 	parser.add_argument('-l', "--log", help="gevent logging", default=None)
+	parser.add_argument('-f', "--fork", help="Set it to make the process go bg", action='store_true')
 	args = parser.parse_args()
 	
+	pid = os.getpid()
 	app = bobo.Application(bobo_resources="disputio.routes")
-	wsgi.WSGIServer((args.address, args.port), application=app, spawn=None, log=args.log).serve_forever()
+	app_server = wsgi.WSGIServer((args.address, args.port), application=app, log=args.log)
+	print('PID: %s'%pid)
+	if args.fork:
+		print('Forking')
+		try:
+			pid = os.fork()
+			print('CHILD PID: %s'%pid)
+		except OSError as e:
+			print('Failed to fork: %s'%e)
+	if args.fork:
+		if pid == 0:
+			os.setsid()
+			app_server.serve_forever()
+	else:
+		app_server.serve_forever()
 	
 	
 if __name__ == '__main__':
